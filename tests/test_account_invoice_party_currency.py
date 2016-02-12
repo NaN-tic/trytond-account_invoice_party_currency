dc5a5de69317110668ac892266f71987d3d4d5bf
Sergi Almacellas Abellana <sergi@koolpi.com>
2016-02-12 20:14:04 +0100

    Migrate to version 3.8 (grafted from 919a7c4dfcffa0e2e6d7b42f464c49f4ae894dd5)

--- tests/test_account_invoice_party_currency.py
+++ tests/test_account_invoice_party_currency.py
@@ -1,27 +1,15 @@
-#!/usr/bin/env python
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import doctest
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
 
 
-class TestCase(unittest.TestCase):
+class TestCase(ModuleTestCase):
     'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module(
-            'account_invoice_party_currency')
-
-    def test0005views(self):
-        'Test views'
-        test_view('account_invoice_party_currency')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'account_invoice_party_currency'
 
 
 def suite():
